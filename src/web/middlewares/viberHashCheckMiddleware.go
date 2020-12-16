97b542e5ffc6d4b95df6e86e9832b5f02a0eab05
Andrii.Nadezhdin <triviy@gmail.com>
2020-12-16 17:42:44 +0200

    add logging

--- src/web/middlewares/viberHashCheckMiddleware.go
+++ src/web/middlewares/viberHashCheckMiddleware.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
 	"github.com/pkg/errors"
 )
 
@@ -16,19 +17,25 @@ func ViberHashCheck(apiKey string) echo.MiddlewareFunc {
 	return middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
 		KeyLookup: "header:X-Viber-Content-Signature",
 		Validator: func(actualHash string, e echo.Context) (bool, error) {
+			log.Info("In KeyAuthWithConfig")
 			mac := hmac.New(sha256.New, []byte(apiKey))
+			log.Info("In KeyAuthWithConfig.Request.GetBody")
 			body, err := e.Request().GetBody()
 			defer body.Close()
-
+			log.Info("After KeyAuthWithConfig.Request.GetBody")
 			if err != nil {
 				return false, errors.Wrap(err, "getting body from request failed")
 			}
+			log.Info("In KeyAuthWithConfig.ReadAll")
 			b, err := ioutil.ReadAll(body)
 			if err != nil {
 				return false, errors.Wrap(err, "getting bytes from request body failed")
 			}
+			log.Info("In KeyAuthWithConfig.Write")
 			mac.Write(b)
+			log.Info("In KeyAuthWithConfig.EncodeToString")
 			expectedHash := hex.EncodeToString(mac.Sum(nil))
+			log.Info("In KeyAuthWithConfig.hmac.Equal")
 			return hmac.Equal([]byte(actualHash), []byte(expectedHash)), nil
 		},
 	})
