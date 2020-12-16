01c115fa45664f7b69ac5729253200d493d3dbd5
Andrii.Nadezhdin <triviy@gmail.com>
2020-12-16 03:03:31 +0200

    fix hash calc

--- src/web/middlewares/viberHashCheckMiddleware.go
+++ src/web/middlewares/viberHashCheckMiddleware.go
@@ -8,29 +8,21 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"github.com/sirupsen/logrus"
 )
 
 // ViberHashCheck gets middleware for validating Viber callbacks
 func ViberHashCheck(apiKey string) echo.MiddlewareFunc {
 	return middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
 		KeyLookup: "header:X-Viber-Content-Signature",
-		Validator: func(hash string, e echo.Context) (bool, error) {
-			logrus.Infof("Got hash in request: %s", hash)
-
+		Validator: func(actualHash string, e echo.Context) (bool, error) {
 			mac := hmac.New(sha256.New, []byte(apiKey))
 			b, err := ioutil.ReadAll(e.Request().Body)
 			if err != nil {
-				logrus.Infof("Error while reading body: %v", err)
 				return false, err
 			}
 			mac.Write(b)
-			expectedHash := mac.Sum(nil)
-			logrus.Infof("Body: %v", string(b))
-			logrus.Infof("Expected hash: %s", string(expectedHash))
-			logrus.Infof("Expected encoded hash: %s", hex.EncodeToString(expectedHash))
-
-			return hmac.Equal([]byte(hash), expectedHash), nil
+			expectedHash := hex.EncodeToString(mac.Sum(nil))
+			return hmac.Equal([]byte(actualHash), []byte(expectedHash)), nil
 		},
 	})
 }
