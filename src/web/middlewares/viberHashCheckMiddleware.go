963fe5ea58db6eceea5d84f101d635b69b462784
Andrii.Nadezhdin <triviy@gmail.com>
2020-12-16 12:54:20 +0200

    fix getting body

--- src/web/middlewares/viberHashCheckMiddleware.go
+++ src/web/middlewares/viberHashCheckMiddleware.go
@@ -1,15 +1,14 @@
 package middlewares
 
 import (
-	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
-	"io"
 	"io/ioutil"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/pkg/errors"
 )
 
 // ViberHashCheck gets middleware for validating Viber callbacks
@@ -18,13 +17,13 @@ func ViberHashCheck(apiKey string) echo.MiddlewareFunc {
 		KeyLookup: "header:X-Viber-Content-Signature",
 		Validator: func(actualHash string, e echo.Context) (bool, error) {
 			mac := hmac.New(sha256.New, []byte(apiKey))
-
-			var buf bytes.Buffer
-			tee := io.TeeReader(e.Request().Body, &buf)
-			b, err := ioutil.ReadAll(tee)
-
+			body, err := e.Request().GetBody()
 			if err != nil {
-				return false, err
+				return false, errors.Wrap(err, "getting body from request failed")
+			}
+			b, err := ioutil.ReadAll(body)
+			if err != nil {
+				return false, errors.Wrap(err, "getting bytes from request body failed")
 			}
 			mac.Write(b)
 			expectedHash := hex.EncodeToString(mac.Sum(nil))
