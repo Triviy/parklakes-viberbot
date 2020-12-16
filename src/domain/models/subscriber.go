50f2b928c0873ddaabb5f0854ad158c407b31381
Andrii.Nadezhdin <triviy@gmail.com>
2020-12-17 00:20:30 +0200

    add ommitempty to slice

--- src/domain/models/subscriber.go
+++ src/domain/models/subscriber.go
@@ -2,10 +2,10 @@ package models
 
 // Subscriber of Viber bot
 type Subscriber struct {
-	ID           string   `bson:"_id"`
+	ID           string   `bson:"_id,omitempty"`
 	Name         string   `bson:"name"`
 	Avatar       string   `bson:"avatar,omitempty"`
 	Country      string   `bson:"country,omitempty"`
-	PhoneNumbers []string `bson:"phoneNumbers"`
+	PhoneNumbers []string `bson:"phoneNumbers,omitempty"`
 	Active       bool     `bson:"active"`
 }
